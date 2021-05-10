2ae31be8bb1fc4680558066c94c4d34319be27fa
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-10 03:34:07 -0400

    image added

--- src/Components/CompanyInfo.js
+++ src/Components/CompanyInfo.js
@@ -2,102 +2,58 @@ import React from "react";
 
 const CompanyInfo = () => {
   return (
-    <>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-10 lg:grid-cols-2">
-          <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
-              <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points=" 8,5 8,1 16,1 16,5"
-                  strokeLinejoin="round"
-                />
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points="9,15 1,15 1,5 23,5 23,15 15,15"
-                  strokeLinejoin="round"
-                />
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  points="22,18 22,23 2,23 2,18"
-                  strokeLinejoin="round"
-                />
-                <rect
-                  x="9"
-                  y="13"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  width="6"
-                  height="4"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-            <div className="max-w-xl mb-6">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                Let us handle
-                <br className="hidden md:block" />
-                your next{" "}
-                <span className="inline-block text-deep-purple-accent-400">
-                  destination
-                </span>
+    <section className="space-y-5">
+      <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className=" absolute inset-0">
+          <div className="absolute inset-y-0 z-0 w-full h-full rounded-lg bg-gray-100 lg:w-3/4" />
+        </div>
+        <div className="relative">
+          <div className="grid gap-12 row-gap-8 lg:grid-cols-2">
+            <div className="grid md:grid-cols-1">
+              {/* <div className="relative">
+                 <svg
+                  viewBox="0 0 52 24"
+                  fill="currentColor"
+                  className="absolute top-0 left-0 z-0 w-32 -mt-8 -ml-16 text-blue-gray-100 lg:w-32 lg:-mt-12"
+                >
+                  <defs>
+                    <pattern
+                      id="d06ca312-d4ed-465f-ad18-fb0c0f92b6f1"
+                      x="0"
+                      y="0"
+                      width=".135"
+                      height=".30"
+                    >
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect
+                    fill="url(#d06ca312-d4ed-465f-ad18-fb0c0f92b6f1)"
+                    width="52"
+                    height="24"
+                  />
+                </svg> 
+              </div> */}
+              <h2 className="font-heading text-center text-4xl sm:text-5xl md:text-5xl 2xl:text-7xl font-semibold title-font">
+                Coffee Roasters{" "}
               </h2>
-              <p className="text-base text-gray-700 md:text-lg">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                quae. explicabo.
+              <p>
+                We are an independently owned and operated specialty coffee
+                roastery and coffee house. We started in 2013 with the
+                dedication to source only the highest quality coffee from farms
+                around the world right back to our shop and into your hands.
+                Freshly roasted with meticulous care at our Toronto roastery,
+                our approachable and accessible coffees bring crisp flavor and
+                smiles with every sip. Our commitment to support farmers and
+                develop our trade is at the centre of everything we do. We
+                believe in and promote respect and excellence from crop to café
+                to cup.
               </p>
             </div>
             <div>
-              <a
-                href="/"
-                aria-label=""
-                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-              >
-                Learn more
-                <svg
-                  className="inline-block w-3 ml-2"
-                  fill="currentColor"
-                  viewBox="0 0 12 12"
-                >
-                  <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="flex items-center justify-center -mx-4 lg:pl-8">
-            <div className="flex flex-col items-end px-3">
               <img
-                className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-                src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt=""
-              />
-              <img
-                className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-                src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt=""
-              />
-            </div>
-            <div className="px-3">
-              <img
-                className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-                src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+                src="/owners.jpg"
                 alt=""
               />
             </div>
@@ -105,140 +61,147 @@ const CompanyInfo = () => {
         </div>
       </div>
 
-      <div class="max-w-screen-xl p-4 dark:bg-gray-800 mx-auto px-4 sm:px-6 lg:px-8 relative py-26 lg:mt-20">
-        <div class="relative">
-          <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
-            <div class="lg:col-start-2 lg:max-w-2xl ml-auto">
-              <p class="text-base leading-6 text-indigo-500 font-semibold uppercase">
-                Interactive
-              </p>
-              <h4 class="mt-2 text-2xl leading-8 font-extrabold text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
-                Interactivity between team members is the key of the success.
-              </h4>
-              <p class="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
-                Build a simply and powered collaborative space for all your
-                team. Track, share, measure, you have a fully control, it&#x27;s
-                never be simply and efficient.
-              </p>
-              <ul class="mt-8 md:grid md:grid-cols-2 gap-6">
-                <li class="mt-6 lg:mt-0">
-                  <div class="flex">
-                    <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        class="h-4 w-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                      Live modifications
-                    </span>
-                  </div>
-                </li>
-                <li class="mt-6 lg:mt-0">
-                  <div class="flex">
-                    <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        class="h-4 w-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                      Data tracker
-                    </span>
-                  </div>
-                </li>
-                <li class="mt-6 lg:mt-0">
-                  <div class="flex">
-                    <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        class="h-4 w-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                      24/24 support
-                    </span>
-                  </div>
-                </li>
-                <li class="mt-6 lg:mt-0">
-                  <div class="flex">
-                    <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
-                      <svg
-                        class="h-4 w-4"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                      Free tips to improve work time
-                    </span>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div class="mt-10 lg:-mx-4 relative relative-20 lg:mt-0 lg:col-start-1">
-              <div class="relative space-y-4">
-                <div class="flex items-end justify-center lg:justify-start space-x-4">
-                  <img
-                    class="rounded-lg shadow-lg w-32 md:w-56"
-                    width="200"
-                    src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                    alt="1"
+      {/* NEW SECTION */}
+
+      <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="absolute inset-0">
+          <div className="absolute inset-y-0 z-0 w-full h-full bg-gray-100 lg:w-3/4" />
+        </div>
+        <div className="relative">
+          <div className="grid gap-12 row-gap-8 lg:grid-cols-2">
+            <div className="grid gap-12 row-gap-5 md:grid-cols-2">
+              <div className="relative">
+                <svg
+                  viewBox="0 0 52 24"
+                  fill="currentColor"
+                  className="absolute top-0 left-0 z-0 w-32 -mt-8 -ml-16 text-blue-gray-100 lg:w-32 lg:-mt-12"
+                >
+                  <defs>
+                    <pattern
+                      id="d06ca312-d4ed-465f-ad18-fb0c0f92b6f1"
+                      x="0"
+                      y="0"
+                      width=".135"
+                      height=".30"
+                    >
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect
+                    fill="url(#d06ca312-d4ed-465f-ad18-fb0c0f92b6f1)"
+                    width="52"
+                    height="24"
                   />
-                  <img
-                    class="rounded-lg shadow-lg w-40 md:w-64"
-                    width="260"
-                    src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                    alt="2"
-                  />
-                </div>
-                <div class="flex items-start justify-center lg:justify-start space-x-4 ml-12">
-                  <img
-                    class="rounded-lg shadow-lg w-24 md:w-40"
-                    width="170"
-                    src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                    alt="3"
-                  />
-                  <img
-                    class="rounded-lg shadow-lg w-32 md:w-56"
-                    width="200"
-                    src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                    alt="4"
-                  />
+                </svg>
+                <div className="relative">
+                  <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-teal-accent-400">
+                    <svg
+                      className="w-8 h-8 text-teal-900"
+                      stroke="currentColor"
+                      viewBox="0 0 52 52"
+                    >
+                      <polygon
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        fill="none"
+                        points="29 13 14 29 25 29 23 39 38 23 27 23"
+                      />
+                    </svg>
+                  </div>
+                  <h6 className="mb-2 font-semibold leading-5">
+                    The quick, brown fox jumps over a lazy dog
+                  </h6>
+                  <p className="text-sm text-gray-900">
+                    Baseball ipsum dolor sit amet cellar rubber win hack tossed.
+                    Slugging catcher slide bench league.
+                  </p>
                 </div>
               </div>
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-teal-accent-400">
+                  <svg
+                    className="w-8 h-8 text-teal-900"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </div>
+                <h6 className="mb-2 font-semibold leading-5">
+                  A flower in my garden, a mystery
+                </h6>
+                <p className="text-sm text-gray-900">
+                  They urge you to put down your sword and come join the
+                  winners.
+                </p>
+              </div>
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-teal-accent-400">
+                  <svg
+                    className="w-8 h-8 text-teal-900"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </div>
+                <h6 className="mb-2 font-semibold leading-5">
+                  Skate ipsum dolor sit amet, alley oop
+                </h6>
+                <p className="text-sm text-gray-900">
+                  If one examines precultural libertarianism, one is faced with
+                  a choice: either accept rationalism.
+                </p>
+              </div>
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-teal-accent-400">
+                  <svg
+                    className="w-8 h-8 text-teal-900"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </div>
+                <h6 className="mb-2 font-semibold leading-5">
+                  The first mate and his Skipper too will
+                </h6>
+                <p className="text-sm text-gray-900">
+                  Those options are already baked in with this model shoot me an
+                  email clear blue water.
+                </p>
+              </div>
+            </div>
+            <div>
+              <img
+                className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+                src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
