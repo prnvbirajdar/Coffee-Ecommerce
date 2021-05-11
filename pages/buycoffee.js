ccc870eb95e1c875ccd9ff7a4ed88acd1ba037e3
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-11 01:14:20 -0400

    individual coffee page small

--- pages/buycoffee.js
+++ pages/buycoffee.js
@@ -3,99 +3,87 @@ import React from "react";
 export default function buycoffee() {
   return (
     <>
-      <section>
-                div
+      <section className="space-y-8 py-32 flex flex-col md:flex-row justify-center items-center min-h-screen w-10/12 max-w-md mx-auto ">
+        <div className="block md:hidden w-full space-y-2 ">
+          <h1 className="font-heading text-3xl title-font font-medium uppercase">
+            Ethiopia
+          </h1>
+          <p className="text-end uppercase text-sm 2xl:text-base">
+            Fruity • Bright
+          </p>
+          <p>$ 12.99</p>
+        </div>
 
-
-      </section>
-
-      <section class=" body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="lg:w-4/5 mx-auto flex flex-wrap">
-            <img
-              alt="gallery"
-              className="mx-auto rounded-lg w-auto h-96 object-cover object-center"
-              src="/parasol-coffee-bags-1.jpg"
-            />
-            {/* <img
-            alt="ecommerce"
-            class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+        <div>
+          <img
+            alt="gallery"
+            className="mx-auto rounded-lg w-auto h-96 object-cover object-center"
             src="/parasol-coffee-bags-1.jpg"
-          /> */}
-            <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                BRAND NAME
-              </h2>
-              <h1 class=" text-3xl title-font font-medium mb-1">
-                The Catcher in the Rye
-              </h1>
-
-              <p class="leading-relaxed">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean shorts keytar banjo tattooed umami
-                cardigan.
-              </p>
-              {/* <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                <div class="flex">
-                  <span class="mr-3">Color</span>
-                  <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                </div>
-                <div class="flex ml-6 items-center">
-                  <span class="mr-3">Size</span>
-                  <div class="relative">
-                    <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </div> */}
-              {/* <div class="flex">
-                <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
-                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button>
-              </div> */}
-            </div>
-          </div>
+          />
         </div>
 
-        <div className="text-black px-5">
-          <p className="px-5 rounded py-1 bg-gray-300 block sm:hidden">
-            Mobile
-          </p>
-          <p className="px-5 rounded py-1 bg-red-300  hidden sm:block md:hidden">
-            Sm
-          </p>
-          <p className="px-5 rounded py-1 bg-green-300 hidden  sm:hidden md:block  lg:hidden">
-            Md
-          </p>
-          <p className="px-5 rounded py-1 bg-blue-300 hidden sm:hidden md:hidden lg:block xl:hidden">
-            Lg
-          </p>
-          <p className="px-5 rounded py-1 bg-yellow-300 hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden">
-            xl
-          </p>
-          <p className="px-5 rounded py-1 bg-purple-300 hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block">
-            2xl
+        <div className=" mx-auto space-y-3">
+          <div className="hidden md:block">
+            <h1 className="text-3xl title-font font-medium mb-1">ETHIOPIA </h1>
+
+            <p className="text-end uppercase text-sm 2xl:text-base">
+              Fruity • Bright
+            </p>
+
+            <p className="">$ 12.99</p>
+          </div>
+          <div className="space-y-3">
+            <label className="pr-1">Quantity </label>{" "}
+            <input
+              type="number"
+              className="rounded-lg w-16 p-3"
+              defaultValue="1"
+            />
+            <a
+              href="#"
+              className="text-center shadow-lg md:text-left font-heading block bg-white hover:bg-gray-200 transition-colors py-3 px-5 rounded-lg text-lg text-gray-800 font-bold "
+            >
+              Add To Cart
+            </a>
+            <a
+              href="#"
+              className="text-center shadow-lg md:text-left font-heading block bg-white hover:bg-gray-200 transition-colors py-3 px-5 rounded-lg text-lg text-gray-800 font-bold "
+            >
+              Buy Now
+            </a>{" "}
+          </div>
+          <div>
+            <hr className="my-6 border-1 border-gray-200 " />
+          </div>
+          <p className="leading-relaxed">
+            Well-balanced and accessible, this single-origin coffee from
+            Ethiopia has a full-body with nutty tones and flavors ranging from
+            sweet to spicy. The combination of acidity with its deep penetrating
+            taste and floral notes provide a complex aroma, which covers the
+            full palette. It has every aroma that you would expect from a good
+            coffee, from sour to bitter in perfect balance.
           </p>
         </div>
       </section>
+
+      <div className="text-black px-5">
+        <p className="px-5 rounded py-1 bg-gray-300 block sm:hidden">Mobile</p>
+        <p className="px-5 rounded py-1 bg-red-300  hidden sm:block md:hidden">
+          Sm
+        </p>
+        <p className="px-5 rounded py-1 bg-green-300 hidden  sm:hidden md:block  lg:hidden">
+          Md
+        </p>
+        <p className="px-5 rounded py-1 bg-blue-300 hidden sm:hidden md:hidden lg:block xl:hidden">
+          Lg
+        </p>
+        <p className="px-5 rounded py-1 bg-yellow-300 hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden">
+          xl
+        </p>
+        <p className="px-5 rounded py-1 bg-purple-300 hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block">
+          2xl
+        </p>
+      </div>
     </>
   );
 }
